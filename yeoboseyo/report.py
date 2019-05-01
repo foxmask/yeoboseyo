5cb1dd4d639c6754d7d936bd1cd61f3931ce03cb
foxmask <foxmaskhome@gmail.com>
2019-05-01 13:22:17 +0200

    display list of trigger and deal with on/off status

---
+++ yeoboseyo/report.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import asyncio
+from yeoboseyo.models import Trigger
+
+
+async def report():
+    triggers = await Trigger.objects.all()
+    print("{:<5} {:<30} {:<22} {:<30} {:<6}".format("ID", "Name", "Triggered", "Notebook", "Status"))
+    for trigger in triggers:
+        fill = '      '
+        print("{:5} {:<30} {:%Y-%m-%d %H:%M}{} {:<30} {:<6}".format(trigger.id,
+                                                                    trigger.description,
+                                                                    trigger.date_triggered,
+                                                                    fill,
+                                                                    trigger.joplin_folder,
+                                                                    trigger.status))
+
+if __name__ == '__main__':
+    print('여보세요 ! Report')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(report())
+    loop.close()
---
+++ yeoboseyo/switch.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import argparse
+import asyncio
+from yeoboseyo.models import Trigger
+
+
+async def switch(trigger_id):
+    trigger = await Trigger.objects.get(id=trigger_id)
+    status = not trigger.status
+    await trigger.update(status=status)
+    print(f"Successfully switched Trigger '{trigger.description}' to {status}")
+
+
+if __name__ == '__main__':
+    print('여보세요 ! Switch')
+    parser = argparse.ArgumentParser(description='Switch status of one triggers.')
+    parser.add_argument('trigger_id',
+                        metavar='N',
+                        type=int,
+                        help='provide the id of the trigger to switch on/off')
+    args = parser.parse_args()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(switch(args.trigger_id))
+    loop.close()
