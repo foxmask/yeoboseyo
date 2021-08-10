f2e276279a540bf9eea4c3676fdce01065b58128
FoxMaSk <foxmask@protonmail.com>
2021-08-10 14:19:31 +0200

    TU

---
+++ yeoboseyo/test_settings.py
@@ -0,0 +1,50 @@
+import databases
+import os
+from starlette.datastructures import Secret
+import sys
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_FOLDER = os.path.dirname(PROJECT_DIR)
+sys.path.append(PARENT_FOLDER)
+
+from yeoboseyo import settings
+
+
+def test_config():
+    """
+    testing settings of the app
+    :return:
+    """
+    assert hasattr(settings, 'DEBUG')
+    assert hasattr(settings, 'DATABASE_URL')
+    assert hasattr(settings, 'YEOBOSEYO_HOST')
+    assert hasattr(settings, 'YEOBOSEYO_PORT')
+    assert hasattr(settings, 'TIME_ZONE')
+    assert hasattr(settings, 'FORMAT_FROM')
+    assert hasattr(settings, 'FORMAT_TO')
+    assert hasattr(settings, 'BYPASS_BOZO')
+    assert hasattr(settings, 'MASTODON_USERNAME')
+    assert hasattr(settings, 'MASTODON_PASSWORD')
+    assert hasattr(settings, 'MASTODON_INSTANCE')
+    assert hasattr(settings, 'MASTODON_VISIBILITY')
+    assert hasattr(settings, 'MY_NOTES_FOLDER')
+    assert hasattr(settings, 'TEMPLATE_STYLE')
+    assert hasattr(settings, 'TELEGRAM_TOKEN')
+    assert hasattr(settings, 'TELEGRAM_CHAT_ID')
+    assert hasattr(settings, 'TOKEN')
+
+    assert type(settings.DEBUG) is bool
+    assert type(settings.DATABASE_URL) is databases.DatabaseURL
+    assert type(settings.YEOBOSEYO_HOST) is str
+    assert type(settings.YEOBOSEYO_PORT) is int
+    assert type(settings.TIME_ZONE) is str
+    assert type(settings.FORMAT_FROM) is str
+    assert type(settings.FORMAT_TO) is str
+    assert type(settings.BYPASS_BOZO) is bool
+    assert type(settings.MASTODON_USERNAME) is str
+    assert type(settings.MASTODON_PASSWORD) is Secret
+    assert type(settings.MASTODON_INSTANCE) is str
+    assert type(settings.MY_NOTES_FOLDER) is str
+    assert type(settings.TELEGRAM_TOKEN) is Secret
+    assert type(settings.TELEGRAM_CHAT_ID) is Secret
+    assert type(settings.TOKEN) is Secret
