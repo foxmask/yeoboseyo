7a198d47d2b85f570c9f9ee65ebb3173e5eb8cfa
FoxMaSk <foxmask@protonmail.com>
2021-08-14 15:49:18 +0200

    0.6.1

--- yeoboseyo/__init__.py
+++ yeoboseyo/__init__.py
@@ -2,9 +2,12 @@
 """
    여보세요
 """
+import importlib_metadata
+
+__version__ = importlib_metadata.version('yeoboseyo')
+
 
 from yeoboseyo.services import Service
 from yeoboseyo.forms import TriggerSchema
 from yeoboseyo.models import Trigger
 
-__version__ = '0.6.0'
